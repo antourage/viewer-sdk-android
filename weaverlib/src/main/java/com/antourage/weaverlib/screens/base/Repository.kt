4911d13689bacdf191e8c3675a33d730184aa0de
Mariana <mk@leobit.com>
2019-08-28 16:10:08 +0300

    Mocked VOD fixed

--- weaverlib/src/main/java/com/antourage/weaverlib/screens/base/Repository.kt
+++ weaverlib/src/main/java/com/antourage/weaverlib/screens/base/Repository.kt
@@ -29,83 +29,91 @@ class Repository @Inject constructor(val api: WebService) {
         val baseUrl = "http://18.185.126.11:1935/vod/"
         list.add(
             StreamResponse(
-                1,
-                "Inside England, with Vauxhall",
-                "Heading Out for Training",
-                "",
-                "file:///android_asset/1.png",
-                subtractDays(0),
-                arrayOf("http://d382pphprxgdpj.cloudfront.net/ant_api_target_dev_8/channels/000003/index.m3u8"),
+                streamId = 1,
+                streamTitle = "Inside England, with Vauxhall",
+                creatorFullName = "Heading Out for Training",
+                creatorNickname = "",
+                thumbnailUrl = "file:///android_asset/1.png",
+                startTime = subtractDays(0),
+                hlsUrl = arrayOf("http://d382pphprxgdpj.cloudfront.net/ant_api_target_dev_8/channels/000003/index.m3u8"),
                 duration = 30,
                 viewerCounter = 13553
             )
         )
         list.add(
             StreamResponse(
-                2, "Roommates with Head and Shoulders", "Kyle Walker and John Stones", "",
-                subtractDays(0, 60 * 60 * 4000),
-                "file:///android_asset/2.png", arrayOf(baseUrl + "2. Roommates.mp4/playlist.m3u8"),
+                streamId = 2,
+                streamTitle = "Roommates with Head and Shoulders",
+                creatorFullName = "Kyle Walker and John Stones",
+                creatorNickname = "",
+                thumbnailUrl = "file:///android_asset/2.png",
+                startTime = subtractDays(0, 60 * 60 * 4000),
+                hlsUrl = arrayOf(baseUrl + "2. Roommates.mp4/playlist.m3u8"),
                 duration = 19,
                 viewerCounter = 14555
             )
         )
         list.add(
             StreamResponse(
-                3,
-                "Live Coverage",
-                "England Women vs Sweden Women",
-                "",
-                "file:///android_asset/3.png",
-                subtractDays(1, 60 * 60 * 4000),
-                arrayOf(baseUrl + "3. England v Sweden LIVE.mp4/playlist.m3u8"),
+                streamId = 3,
+                streamTitle = "Live Coverage",
+                creatorFullName = "England Women vs Sweden Women",
+                creatorNickname = "",
+                thumbnailUrl = "file:///android_asset/3.png",
+                startTime = subtractDays(1, 60 * 60 * 4000),
+                hlsUrl = arrayOf(baseUrl + "3. England v Sweden LIVE.mp4/playlist.m3u8"),
                 duration = 26,
                 viewerCounter = 14235
             )
         )
         list.add(
             StreamResponse(
-                4,
-                "Stars of the Future with Nike",
-                "U21 5-a-side game",
-                "",
-                "file:///android_asset/4.png",
-                subtractDays(2),
-                arrayOf(baseUrl + "4. England U21 5_a_side.mp4/playlist.m3u8"),
+                streamId = 4,
+                streamTitle = "Stars of the Future with Nike",
+                creatorFullName = "U21 5-a-side game",
+                creatorNickname = "",
+                thumbnailUrl = "file:///android_asset/4.png",
+                startTime = subtractDays(2),
+                hlsUrl = arrayOf(baseUrl + "4. England U21 5_a_side.mp4/playlist.m3u8"),
                 duration = 19,
                 viewerCounter = 4324
             )
         )
         list.add(
             StreamResponse(
-                5,
-                "SupporterReporter",
-                "Penalties vs Columbia",
-                "",
-                "file:///android_asset/5.png",
-                subtractDays(3),
-                arrayOf(baseUrl + "5. %23SupporterReporter vs Columbia.mp4/playlist.m3u8"),
+                streamId = 5,
+                streamTitle = "SupporterReporter",
+                creatorFullName = "Penalties vs Columbia",
+                creatorNickname = "",
+                thumbnailUrl = "file:///android_asset/5.png",
+                startTime = subtractDays(3),
+                hlsUrl = arrayOf(baseUrl + "5. %23SupporterReporter vs Columbia.mp4/playlist.m3u8"),
                 duration = 29,
                 viewerCounter = 54354
             )
         )
         list.add(
             StreamResponse(
-                6, "Media Day with M&S", "The squad has been selected", "",
-                subtractDays(4),
-                "file:///android_asset/6.png", arrayOf(baseUrl + "6. Media Day.mp4/playlist.m3u8"),
+                streamId = 6,
+                streamTitle = "Media Day with M&S",
+                creatorFullName = "The squad has been selected",
+                creatorNickname = "",
+                startTime = subtractDays(4),
+                thumbnailUrl = "file:///android_asset/6.png",
+                hlsUrl = arrayOf(baseUrl + "6. Media Day.mp4/playlist.m3u8"),
                 duration = 35,
                 viewerCounter = 234
             )
         )
         list.add(
             StreamResponse(
-                7,
-                "Bud Light’s Alternative Commentary",
-                "FT Reev and Theo Baker",
-                "",
-                "file:///android_asset/7.png",
-                subtractDays(6),
-                arrayOf(baseUrl + "7. Alternative Commentary, FT Reev and Theo Baker.mp4/playlist.m3u8"),
+                streamId = 7,
+                streamTitle = "Bud Light’s Alternative Commentary",
+                creatorFullName = "FT Reev and Theo Baker",
+                creatorNickname = "",
+                thumbnailUrl = "file:///android_asset/7.png",
+                startTime = subtractDays(6),
+                hlsUrl = arrayOf(baseUrl + "7. Alternative Commentary, FT Reev and Theo Baker.mp4/playlist.m3u8"),
                 duration = 40,
                 viewerCounter = 123
             )
