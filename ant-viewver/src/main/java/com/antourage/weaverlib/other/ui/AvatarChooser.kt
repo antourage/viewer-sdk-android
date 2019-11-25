7e5a1af80973cc1ff44ce6125452e8bf549e7448
antourage <52660451+antourage@users.noreply.github.com>
2019-11-25 12:24:17 +0200

    Fixed providers crash
    # Conflicts:
    #	app/build.gradle

--- ant-viewver/src/main/java/com/antourage/weaverlib/other/ui/AvatarChooser.kt
+++ ant-viewver/src/main/java/com/antourage/weaverlib/other/ui/AvatarChooser.kt
@@ -17,7 +17,6 @@ import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v4.content.FileProvider
 import android.support.v7.app.AlertDialog
-import com.antourage.weaverlib.BuildConfig
 import com.antourage.weaverlib.R
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
