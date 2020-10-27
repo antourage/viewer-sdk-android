981c7ecc550ecfd9a1c6c6e12dc0b9bc5527f156
dannie <qqqq1111>
2020-10-27 14:33:11 +0200

    Another improvement for a dialog cancellable outside.

--- ant-viewver/src/main/java/com/antourage/weaverlib/screens/list/dev_settings/DevSettingsDialog.kt
+++ ant-viewver/src/main/java/com/antourage/weaverlib/screens/list/dev_settings/DevSettingsDialog.kt
@@ -77,6 +77,7 @@ internal class DevSettingsDialog(
         handler is used here to prevent immediate dialog closing in case user made more clicks
          than needed to open it.
          */
+        setCanceledOnTouchOutside(false)
         Handler().postDelayed({
             setCanceledOnTouchOutside(true)
         }, 1500)
