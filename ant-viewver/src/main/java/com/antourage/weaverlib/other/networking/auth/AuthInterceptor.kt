e23b34c6e01b13164412cc7926f823b54cb24523
nazar_mykhailevych <nazarrrk0@gmail.com>
2021-04-28 17:07:13 +0300

    - fix statistic localization data

--- ant-viewver/src/main/java/com/antourage/weaverlib/other/networking/auth/AuthInterceptor.kt
+++ ant-viewver/src/main/java/com/antourage/weaverlib/other/networking/auth/AuthInterceptor.kt
@@ -1,16 +1,17 @@
 package com.antourage.weaverlib.other.networking.auth
 
+import android.content.res.Resources
 import android.os.Build
 import com.antourage.weaverlib.BuildConfig
-import com.antourage.weaverlib.Global
 import com.antourage.weaverlib.UserCache
 import com.antourage.weaverlib.other.networking.ApiClient
 import okhttp3.Interceptor
 import okhttp3.Interceptor.Chain
 import okhttp3.Response
 import java.io.IOException
+import java.util.*
 
-class AuthInterceptor: Interceptor {
+class AuthInterceptor : Interceptor {
     companion object {
         private const val HEADER_TOKEN = "Authorization"
         private const val HEADER_LANGUAGE = "Accept-Language"
@@ -32,18 +33,22 @@ class AuthInterceptor: Interceptor {
         val accessToken = UserCache.getInstance()?.getAccessToken()
         if (idToken != null) {
             builder.addHeader(HEADER_TOKEN, "Bearer $idToken")
-        }else if(accessToken!=null){
+        } else if (accessToken != null) {
             builder.addHeader(HEADER_TOKEN, "Bearer $accessToken")
         }
 
-        if(request.url.toString().contains("/open") || request.url.toString().contains("/close")){
+        if (request.url.toString().contains("/open") || request.url.toString().contains("/close")) {
             builder.addHeader(HEADER_OS, "android")
             builder.addHeader(HEADER_VERSION, BuildConfig.VERSION_NAME)
             builder.addHeader(HEADER_OS_VERSION, Build.VERSION.RELEASE)
-            if(Global.chosenLocale!=null){
-                builder.addHeader(HEADER_LOCALIZATION, Global.chosenLocale!!.language)
-            }else if(Global.defaultLocale!=null){
-                builder.addHeader(HEADER_LOCALIZATION, Global.defaultLocale!!.language)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                builder.addHeader(
+                    HEADER_LOCALIZATION,
+                    Resources.getSystem().configuration.locales[0].language
+                )
+            } else {
+                builder.addHeader(HEADER_LOCALIZATION, Locale.getDefault().language)
+
             }
         }
 
@@ -55,7 +60,7 @@ class AuthInterceptor: Interceptor {
             synchronized(ApiClient.getHttpClient()) {
                 val currentIdToken = UserCache.getInstance()?.getIdToken()
                 val currentAccessToken = UserCache.getInstance()?.getAccessToken()
-                if((currentIdToken != null && currentIdToken == idToken) || currentAccessToken == null || currentAccessToken == accessToken) {
+                if ((currentIdToken != null && currentIdToken == idToken) || currentAccessToken == null || currentAccessToken == accessToken) {
                     val code: Int = AuthClient.getAuthClient().authenticateUser().code() / 100
                     if (code != 2) {
                         return response
@@ -64,8 +69,11 @@ class AuthInterceptor: Interceptor {
 
                 if (UserCache.getInstance()?.getIdToken() != null) {
                     builder.header(HEADER_TOKEN, "Bearer ${UserCache.getInstance()?.getIdToken()}")
-                } else if(UserCache.getInstance()?.getAccessToken() != null){
-                    builder.header(HEADER_TOKEN, "Bearer ${UserCache.getInstance()?.getAccessToken()}")
+                } else if (UserCache.getInstance()?.getAccessToken() != null) {
+                    builder.header(
+                        HEADER_TOKEN,
+                        "Bearer ${UserCache.getInstance()?.getAccessToken()}"
+                    )
                 }
                 request = builder.build()
                 return chain.proceed(request)
